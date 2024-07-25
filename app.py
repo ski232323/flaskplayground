9e46c4b05d29df43ee26a21e0d2c74b6a22d55c7
Ulysse Renzetti <ulysse.renzetti@outlook.fr>
2024-07-25 19:51:55 +0200

    jh

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from tinydb import TinyDB, Query
-import json
-from flask import Flask, render_template, request, redirect, url_for
-from werkzeug.security import generate_password_hash, check_password_hash
+from tinydb import TinyDB, Query, where
+import unidecode
 
 db = TinyDB('db.json')
+DATA = Query()
 
-
+el=db.get(DATA.name=='admin')
+el.doc_id()
